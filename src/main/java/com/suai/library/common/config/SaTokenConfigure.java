0a52ff075b2a4d8b1f74bce52b0ed215a3e5dbad
suai <619866035@qq.com>
2025-08-17 18:09:47 +0800

    最终版（应该吧）

--- src/main/java/com/suai/library/common/config/SaTokenConfigure.java
+++ src/main/java/com/suai/library/common/config/SaTokenConfigure.java
@@ -14,23 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {  // 重命名避免冲突
 
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-
-    @Bean
-    public SaTokenConfig getSaTokenConfig() {
-        SaTokenConfig config = new SaTokenConfig();
-        config.setTokenName("Authorization")
-                .setTimeout(60 * 60) // 1小时有效期
-                .setActivityTimeout(30 * 60) // 30分钟无操作过期
-                .setIsShare(true)
-                .setIsReadHeader(true)
-                .setIsReadBody(false)
-                .setIsReadCookie(false)
-                .setTokenStyle("simple-uuid");
-        return config;
-    }
-
     // 注册 Sa-Token 注解拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
